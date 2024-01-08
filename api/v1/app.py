7daf09becfd53ef1abf0f746498c6efe75759b90
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-01-07 18:42:04 -0800

    Reload app.py after reset

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 This sets up the app for the api of the project
 """
-from flask import Flask
+from flask import Flask, Blueprint, request, make_response, jsonify
 from os import getenv
 from models import storage
 from api.v1.views import app_views
@@ -10,6 +10,17 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
+
+
+@app.errorhandler(404)
+def error_404(error):
+    """  create a handler for 404 errors that returns
+    a JSON-formatted 404 status code response."""
+    response = {"error": "Not found"}
+    if request.path.startswith('/api/'):
+        return jsonify(response), error.code
+    else:
+        return error
 
 
 @app.teardown_appcontext
